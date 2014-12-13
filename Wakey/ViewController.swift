94fe7cc3e98b2a0a38543da2e0404d6ab443efae
Sunstroke <snstrk@gmail.com>
2014-12-13 14:38:36 +0300

    Initial Commit

---
+++ Wakey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wakey
+//
+//  Created by Alexander Mazaykin on 13.12.14.
+//  Copyright (c) 2014 Alexander Mazaykin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
